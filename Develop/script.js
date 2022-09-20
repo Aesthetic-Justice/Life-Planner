ff1cba50d89c35101c01fda5f036bda402e1b5a1
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2022-09-19 21:05:02 -0400

    feature: Timeslots assigned correct class by time

--- Develop/script.js
+++ Develop/script.js
@@ -5,17 +5,18 @@ currentDateEl.text('Today\'s date is: ' + currentDateVar);
 
 
 //~~~~ Creates the Calendar ~~~~~~~
-let calendarEl = $('#calendarEl');
-let timeArr = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+const calendarEl = $('#calendarEl');
+const timeArr = [09, 10, 11, 12, 01, 02, 03, 04, 05];
+let currTime = moment().format("H");
 for (let i = 0; i < 9; i++) {
     //Append Row to Grid
     let rowEl = $('<div>');
-    rowEl.attr('class', 'time-block, row');
+    rowEl.attr('class', 'time-block row');
     calendarEl.append(rowEl);
 
     //Append Time Column to Row
     let colEl1 = $('<div>');
-    colEl1.attr('class', 'col, hour');
+    colEl1.attr('class', 'col hour');
     if (i < 4) {
         colEl1.text(timeArr[i] + 'AM');
     }
@@ -26,13 +27,22 @@ for (let i = 0; i < 9; i++) {
 
     //Append Text Column to Row
     let colEl2 = $('<div>');
-    colEl2.attr('class', 'col-10');
+    colEl2.addClass('col-10');
+    if(currTime<i+9){
+        colEl2.addClass('past');
+    }
+    else if(currTime===i+9){
+        colEl2.addClass('present');
+    }
+    else{
+        colEl2.addClass('future');
+    }
     colEl2.text('Test');
     rowEl.append(colEl2);
 
     //Append Button Column to Row
     let colEl3 = $('<button>');
-    colEl3.attr('class', 'col, saveBtn');
+    colEl3.attr('class', 'col saveBtn');
     colEl3.text('Test');
     rowEl.append(colEl3);
 }
