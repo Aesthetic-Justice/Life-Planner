e4bedd6ed10c8ce9b06229c7906fece19e56cbe5
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2022-09-19 21:12:17 -0400

    fix: time check was backwards

--- Develop/script.js
+++ Develop/script.js
@@ -28,7 +28,7 @@ for (let i = 0; i < 9; i++) {
     //Append Text Column to Row
     let colEl2 = $('<div>');
     colEl2.addClass('col-10, textarea');
-    if(currTime<i+9){
+    if(currTime>i+9){
         colEl2.addClass('past');
     }
     else if(currTime===i+9){
